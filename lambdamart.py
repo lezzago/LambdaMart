b10f132e4cc08dde679398ad1425f40700d1485e
aagrawl3 <aagrawl3@illinois.edu>
2016-12-04 17:33:44 -0600

    added parallelization

--- lambdamart.py
+++ lambdamart.py
@@ -2,6 +2,8 @@ import numpy as np
 import math
 import random
 import copy
+from sklearn.tree import DecisionTreeRegressor
+from multiprocessing import Pool
 
 def dcg(scores):
 	return np.sum([
@@ -33,7 +35,32 @@ def delta_ndcg(scores, i, j):
 	ideal_dcg = dcg(ideal_scores)
 	return abs(dcg_val - new_dcg_val)/ideal_dcg
 
-def compute_lambda(true_scores, predicted_scores):
+# useless parallel code
+def lambda_parallel(args):
+	true_scores, good_ij_pairs, predicted_scores = args
+	# print scores
+	# exit()
+
+	# true_scores, predicted_scores = scores
+	i, j = good_ij_pairs
+	z_ndcg = delta_ndcg(true_scores, i, j)
+	rho = 1 / (1 + np.exp(predicted_scores[i] - predicted_scores[j]))
+	rho_complement = 1.0 - rho
+	lambda_val = z_ndcg * rho
+	# lambdas = []
+	# lambdas.append(lambda_val)
+	# lambdas.append(-lambda_val)
+	# w = []
+	w_val = rho * rho_complement * z_ndcg
+	# w.append(w_val)
+	# w.append(w_val)
+	return i, j, lambda_val, w_val
+
+
+#true_scores, predicted_scores
+def compute_lambda(args):
+	true_scores, predicted_scores, good_ij_pairs, query_key = args
+	# true_scores, predicted_scores, good_ij_pairs = scores
 	num_docs = len(true_scores)
 	sorted_indexes = np.argsort(predicted_scores)[::-1]
 	rev_indexes = np.argsort(sorted_indexes)
@@ -42,23 +69,29 @@ def compute_lambda(true_scores, predicted_scores):
 
 	lambdas = np.zeros(num_docs)
 	w = np.zeros(num_docs)
-	temp = set([i for i in xrange(num_docs)])
-	for i in xrange(num_docs):
-		for j in xrange(num_docs):
-			if true_scores[i] > true_scores[j]:
 
-				z_ndcg = delta_ndcg(true_scores, i, j)
-				rho = 1 / (1 + np.exp(predicted_scores[i] - predicted_scores[j]))
-				rho_complement = 1.0 - rho
-				lambda_val = z_ndcg * rho
-				lambdas[i] += lambda_val
-				lambdas[j] -= lambda_val
+	### parallel portion
+	# pool = Pool(processes=8)
+	# for i, j, lambda_val, w_val in pool.map(lambda_parallel, zip([true_scores for i in xrange(len(good_ij_pairs))], good_ij_pairs, [predicted_scores for i in xrange(len(good_ij_pairs))])):
+	# 	lambdas[i] += lambda_val
+	# 	lambdas[j] -= lambda_val
+	# 	w[i] += w_val
+	# 	w[j] += w_val
+	# pool.close()
 
-				w_val = rho * rho_complement * z_ndcg
-				w[i] += w_val
-				w[j] += w_val
+	for i,j in good_ij_pairs:
+		z_ndcg = delta_ndcg(true_scores, i, j)
+		rho = 1 / (1 + np.exp(predicted_scores[i] - predicted_scores[j]))
+		rho_complement = 1.0 - rho
+		lambda_val = z_ndcg * rho
+		lambdas[i] += lambda_val
+		lambdas[j] -= lambda_val
 
-	return lambdas[rev_indexes], w[rev_indexes]
+		w_val = rho * rho_complement * z_ndcg
+		w[i] += w_val
+		w[j] += w_val
+
+	return lambdas[rev_indexes], w[rev_indexes], query_key
 
 def group_queries(training_data):
 	query_indexes = {}
@@ -68,6 +101,18 @@ def group_queries(training_data):
 		query_indexes[record[1]].append(index)
 		index += 1
 	return query_indexes
+
+def get_pairs(scores):
+	query_pair = []
+	for query_scores in scores:
+		temp = sorted(query_scores, reverse=True)
+		pairs = []
+		for i in xrange(len(temp)):
+			for j in xrange(len(temp)):
+				if temp[i] > temp[j]:
+					pairs.append((i,j))
+		query_pair.append(pairs)
+	return query_pair
 
 class LambdaMART:
 
@@ -82,23 +127,40 @@ class LambdaMART:
 		self.learning_rate = learning_rate
 
 	def fit(self):
-		# training_data = np.array(training_data)
-		# predicted_scores = np.array([random.random() for i in xrange(len(self.training_data))])
 		predicted_scores = np.zeros(len(self.training_data))
 		query_indexes = group_queries(self.training_data)
+		query_keys = query_indexes.keys()
+		true_scores = [self.training_data[query_indexes[query], 0] for query in query_keys]
+		good_ij_pairs = get_pairs(true_scores)
 		for k in xrange(self.number_of_trees):
+			print 'Tree %d' % (k)
 			lambdas = np.zeros(len(predicted_scores))
 			w = np.zeros(len(predicted_scores))
-			# for i in xrange(len(self.training_data)):
-			for query in query_indexes:
-				indexes = query_indexes[query]
-				# print len(indexes)
-				# print len(self.training_data[indexes]), len(self.training_data[indexes][1])
-				# print self.training_data[0,1]
-				lambdas[indexes], w[indexes] = compute_lambda(self.training_data[indexes, 0], predicted_scores[indexes])
-			print w
-			exit()
+			pred_scores = [predicted_scores[query_indexes[query]] for query in query_keys]
+			
+			pool = Pool(processes=4)
+			for lambda_val, w_val, query_key in pool.map(compute_lambda, zip(true_scores, pred_scores, good_ij_pairs, query_keys)):
+				indexes = query_indexes[query_key]
+				lambdas[indexes] = lambda_val
+				w[indexes] = w_val
+			pool.close()
 
+			## non parallel
+			# for i in xrange(len(true_scores)):
+			# 	query = query_keys[i]
+			# 	lambdas[query_indexes[query]], w[query_indexes[query]] = compute_lambda(true_scores[i], pred_scores[i], good_ij_pairs[i])
+
+			# sklearn tree			
+			# tree = DecisionTreeRegressor(max_depth=6)
+			# tree.fit(self.training_data[:,2:], lambdas)
+			# prediction = tree.predict(self.training_data[:,2:])
+			# predicted_scores += prediction * self.learning_rate
+
+			###
+			# Tree code here, already calculated lambdas and ws
+			###
+
+		print predicted_scores
 	# def predict(self, data):
 
 
