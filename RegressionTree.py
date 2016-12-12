8e0af9bd789e1454c46abb096dbf38f1884a81a4
aagrawl3 <aagrawl3@illinois.edu>
2016-12-12 17:59:38 -0600

    optimized code

--- RegressionTree.py
+++ RegressionTree.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np 
 from multiprocessing import Pool
 from itertools import repeat
+import scipy
+import scipy.optimize
 
 node_id = 0
 
@@ -27,11 +29,12 @@ def find_best_split_parallel(args):
 	best_children = None
 	split_point, data, label = args
 	key,possible_split = split_point
+
 	for split in possible_split:
 		children = split_children(data, label, key, split)
 
 		#weighted average of left and right ls
-		ls  = float(len(children[1]))/len(label)*least_square(children[1]) + float(len(children[3]))/len(label)*least_square(children[3])
+		ls  = len(children[1])*least_square(children[1])/len(label) + len(children[3])*least_square(children[3])/len(label)
 		if ls < best_ls:
 			best_ls = ls
 			best_split = (key, split)
@@ -44,7 +47,7 @@ def find_best_split(data, label, split_points):
 	best_ls = 1000000
 	best_split = None
 	best_children = None
-
+	print len(split_points.items())
 	pool = Pool()
 	for ls, split, children in pool.map(find_best_split_parallel, zip(split_points.items(), repeat(data), repeat(label))):
 		if ls < best_ls:
@@ -68,8 +71,8 @@ def find_best_split(data, label, split_points):
 	return best_split, best_children # return a tuple(attribute, value)
 
 def split_children(data, label, key, split):
-	left_index = [index for index in range(len(data.iloc[:,key])) if data.iloc[index,key] < split]
-	right_index = [index for index in range(len(data.iloc[:,key])) if data.iloc[index,key] >= split]
+	left_index = [index for index in xrange(len(data.iloc[:,key])) if data.iloc[index,key] < split]
+	right_index = [index for index in xrange(len(data.iloc[:,key])) if data.iloc[index,key] >= split]
 	left_data = data.iloc[left_index,:]
 	right_data = data.iloc[right_index,:]
 	left_label = [label[i] for i in left_index]
@@ -78,12 +81,16 @@ def split_children(data, label, key, split):
 	return left_data, left_label, right_data, right_label 
 
 def least_square(label):
-	# given the 
-	# return the
+	# # given the 
+	# # return the
+	# if not len(label):
+	# 	return 0
+	# # label = np.array(label).astype(float)
+	# return len(label) * np.var(label)
+	# # return np.sum((label - np.mean(label))**2)
 	if not len(label):
 		return 0
-	label = np.array(label).astype(float)
-	return np.sum((label - np.mean(label))**2)
+	return (np.sum(label)**2)/len(set(label))
 
 
 def create_leaf(label):
@@ -97,9 +104,17 @@ def create_leaf(label):
 	leaf['value'] = round(np.mean(label),3)
 	return leaf
 
+def find_splits_parallel(args):
+	var_space, label, col = args
+	# var_space = data.iloc[:,col].tolist()
+	return scipy.optimize.fminbound(error_function, min(var_space), max(var_space), args = (col, var_space, label), full_output = 1)
+	# return, 
+	# if not min_error or error < min_error:
+	# 	min_error = error
+	# 	split_var = col
+	# 	min_split = split
 
 def create_tree(data, all_pos_split, label, max_depth, ideal_ls, current_depth = 0):
-	
 	remaining_features = all_pos_split
 	#stopping conditions
 	if sum([len(v)!= 0 for v in remaining_features.values()]) == 0:
@@ -113,11 +128,43 @@ def create_tree(data, all_pos_split, label, max_depth, ideal_ls, current_depth =
 		print "reached max depth, stop."
 		return create_leaf(label)
 
-	# find splitting features    
-	splitting_feature, children = find_best_split(data, label, remaining_features)  #tuple(id, value)
-	# print "split feature: ", splitting_feature
-	# remove current features
-	remaining_features[splitting_feature[0]].remove(splitting_feature[1])
+	
+	#######
+	min_error = None
+	split_var = None
+	min_split = None
+
+	var_spaces = [data.iloc[:,col].tolist() for col in xrange(data.shape[1])]
+	cols = [col for col in xrange(data.shape[1])]
+	print 'find best split'
+	pool = Pool()
+	for split, error, ierr, numf in pool.map(find_splits_parallel, zip(var_spaces, repeat(label), cols)):
+		if not min_error or error < min_error:
+			min_error = error
+			split_var = col
+			min_split = split
+	pool.close()
+
+	# for col in xrange(data.shape[1]):
+	# 	var_space = data.iloc[:,col].tolist()
+	# 	split, error, ierr, numf = scipy.optimize.fminbound(error_function, min(var_space), max(var_space), args = (col, data.values.tolist(), label), full_output = 1)
+	# 	if not min_error or error < min_error:
+	# 		min_error = error
+	# 		split_var = col
+	# 		min_split = split
+	splitting_feature = (split_var, min_split)
+	#######
+	children = split_children(data, label, split_var, min_split)
+	print 'found best split'
+	# exit()
+
+	# # find splitting features    
+	# print 'find best_split'
+	# splitting_feature, children = find_best_split(data, label, remaining_features)  #tuple(id, value)
+	# print 'found best split'
+	# # print "split feature: ", splitting_feature
+	# # remove current features
+	# remaining_features[splitting_feature[0]].remove(splitting_feature[1])
 	left_data, left_label, right_data, right_label = children
 
 	left_least_square = least_square(left_label)
@@ -144,6 +191,24 @@ def create_tree(data, all_pos_split, label, max_depth, ideal_ls, current_depth =
 			'right'            : right_tree,
 			'index'			   : None}
 
+def error_function(split_point, split_var, data, label):
+	# left_index = [index for index in xrange(len(data.iloc[:,split_var])) if data.iloc[index,split_var] < split_point]
+	# right_index = [index for index in xrange(len(data.iloc[:,split_var])) if data.iloc[index,split_var] >= split_point]
+	# left_data = data.iloc[left_index,:]
+	# right_data = data.iloc[right_index,:]
+	# left_label = [label[i] for i in left_index]
+	# right_label =[label[i] for i in right_index]
+	data1 = []
+	data2 = []
+	for i in xrange(len(data)):
+		temp_dat = data[i]
+		if temp_dat <= split_point:
+			data1.append(label[i])
+		else:
+			data2.append(label[i])
+	return least_square(data1) + least_square(data2)
+
+
 def make_prediction(tree, x, annotate = False):
 	if tree['is_leaf']:
 		if annotate: 
@@ -168,6 +233,8 @@ class RegressionTree:
 		self.tree = None
 
 	def fit(self):
+		global node_id
+		node_id = 0
 		all_pos_split = {}
 		pool = Pool()
 		splitting_data = [self.training_data.iloc[:,col].tolist() for col in xrange(self.training_data.shape[1])]
@@ -175,7 +242,7 @@ class RegressionTree:
 		for dat, col in pool.map(get_splitting_points, zip(splitting_data, cols)):
 			all_pos_split[col] = dat
 		pool.close()
-
+		print 'actually making trees'
 		self.tree = create_tree(self.training_data, all_pos_split, self.labels, self.max_depth, self.ideal_ls)
 
 
